1247f667bd1f3a04778bf012c2e6e3a9dd80658c
mehrdadyo <mehrdad.yousefzadeh@gmail.com>
2020-02-29 19:12:51 -0800

    test files

---
+++ Untitled3.m
@@ -0,0 +1,32 @@
+figure(3)
+contourf(DOMAIN.Xp,DOMAIN.Yp,(StateVar.phi<0).*StateVar.phi.*(LS.psi>0), 50, 'LineStyle', 'none'); colormap jet
+hold on
+plot(LSCase.BoundaryCurve.xt, LSCase.BoundaryCurve.yt+45, 'k')
+hold on
+plot(LSCase.BoundaryCurve.xt, LSCase.BoundaryCurve.yb+45, 'k')
+
+hold on
+scatter(DOMAIN.xp(IBM_coeffP.I1_p), DOMAIN.yp(IBM_coeffP.J1_p))
+hold on 
+scatter(DOMAIN.xp(IBM_coeffP.I2_p), DOMAIN.yp(IBM_coeffP.J2_p))
+hold on 
+scatter(DOMAIN.xp(IBM_coeffP.I3_p), DOMAIN.yp(IBM_coeffP.J3_p))
+hold on 
+scatter(DOMAIN.xp(IBM_coeffP.I4_p), DOMAIN.yp(IBM_coeffP.J4_p))
+hold on 
+scatter(DOMAIN.xp(IBM_coeffP.I_g_p), DOMAIN.yp(IBM_coeffP.J_g_p))
+hold on 
+
+plot(LSCase.BoundaryCurve.xt,LSCase.BoundaryCurve.yb+DOMAIN.ly/2, 'k')
+
+
+    [StateVar,ControlVar] = ... 
+        SolveUVP (ControlVar,Flux,DOMAIN,VARIABLES,StateVar,IBM,IBM_coeffU,...
+        IBM_coeffV,IBM_coeffP,BC);    
+    
+    StateVar.U_old = StateVar.U;
+    StateVar.V_old = StateVar.V;
+    StateVar.P_old = StateVar.P;
+    
+contourf(DOMAIN.Xp,DOMAIN.Yp,(LSinit.psi), 50, 'LineStyle', 'none'); colormap jet
+    
