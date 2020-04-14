c655618503b0f1209a32a364870cc62b79a3568d
Mehrdad Yousefzadeh <mehrdad.yousefzadeh@gmail.com>
2020-04-13 22:52:47 -0700

    Update LSeqSolve.m

--- SolveLS/LSeqSolve.m
+++ SolveLS/LSeqSolve.m
@@ -6,13 +6,15 @@ function [LS] = LSeqSolve(LS,StateVar,VARIABLES,DOMAIN)
 % ONE thing I'm not quite sure about is whether I have to use velocities at
 % the n+1 and n+1/2 in RK stages (to be checked later).
 %% =======================================================================
-
+% extrapolate the velocity
 [LS.u,LS.v] = LSVelocityExtrapolation(VARIABLES, StateVar, DOMAIN, LS);
-
-
-LS.psi = solveHJEq(LS, VARIABLES, DOMAIN);
-
-LS.psi = LSreinitialization(VARIABLES, DOMAIN, LS);
+% save phi_o for getting the N_o the tube for reinitialization eq
+psi_o = LS.psi;
+% solve LS eq to get tilda psi
+LS.psi = solveHJEq(LS, VARIABLES, DOMAIN, psi_o);
+% reinitialize LS to a signed distance function
+LS.psi = LSreinitialization(VARIABLES, DOMAIN, LS, psi_o);
+% compute the normals
 [LS] = LSnormals(LS,DOMAIN);
 
 
